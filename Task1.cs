9ffd79ed4517cae8d458fb333d897f24acda2be9
ravikumarr1802 <162043399+ravikumarr1802@users.noreply.github.com>
2024-08-16 15:30:26 +0530

    Add files via upload

---
+++ Task1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+public class p1{
+    public static void Main(string[] args){
+        int n,rev=0,temp;
+        Console.WriteLine("Enter a number: ");
+        n=Convert.ToInt32(Console.ReadLine());
+        temp=n;
+        while(n>0)
+        {
+            rev=rev*10+n%10;
+            n=n/10;
+        }
+        if(temp==rev)
+        Console.Write("Palindrome");
+        else
+        Console.Write("Not a palindrome");
+    }
+}
